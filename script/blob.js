6c17e29c4ee63f612dc79425ce72262010d881ab
Nyx <56017356+stoolwitch@users.noreply.github.com>
2024-05-21 17:59:11 +0200

    wavy animated section separation

--- script/blob.js
+++ script/blob.js
@@ -1,15 +1,22 @@
 
-const svg = document.getElementById("an")
-const warp = new Warp(svg)
+const desBlob = document.getElementById("desBlob");
+const warpDes = new Warp(desBlob);
 
-warp.interpolate(4)
-warp.transform(([x, y]) => [x, y, y])
+const headerBlob = document.getElementById("headerBlob");
+const warpHeader = new Warp(headerBlob);
 
-let offset = 0
+warpDes.interpolate(4);
+warpDes.transform(([x, y]) => [x, y, y]);
+
+warpHeader.interpolate(4);
+warpHeader.transform(([x, y]) => [x, y, y]);
+
+let offset = 0;
 function animate() {
-    warp.transform(([x, y, oy]) => [x, oy + 4 * Math.sin(x / 20 + offset), oy])
-    offset += 0.01
-    requestAnimationFrame(animate)
-}
+    warpDes.transform(([x, y, oy]) => [x, oy + 4 * Math.sin(x / 50 + offset), oy]);
+    warpHeader.transform(([x, y, oy]) => [x, oy + 1 * Math.sin(x / 10 + offset), oy]);
+    offset += 0.01;
+    requestAnimationFrame(animate);
+};
 
-animate()
+animate();
